add5ad8fd16a6026264591376d1d7d8b458aa3b7
Sarathy <Sarathy@DESKTOP-5RGC7S8>
2020-08-30 18:24:15 +0530

    Testing Code

---
+++ NewProg/src/Test/NewTest.java
@@ -0,0 +1,35 @@
+package Test;
+
+
+
+public class NewTest {
+	
+	Long accno=123456789123456l;
+	
+	Integer accbal=5000;
+	
+	int acb=6000;
+	
+	String aname="karthick";
+	
+	
+  public void accbalane()
+  {
+	  System.out.println(accbal);
+  }
+	
+	public static void main(String args[])
+	{
+		//cname objname=new cname();
+		
+		NewTest s=new NewTest();
+		//s.accbalane();
+	
+		
+		
+		
+		
+		
+	}
+	
+}
